dd1191f4d8ca881297dc0caee6ec0d4e2d7cb151
Gavin Palmer <gavin@herolfg.com>
2020-05-18 17:23:33 -0500

    init commit

---
+++ precache-manifest.87dcfe3bbe5ea798f9e994fee6c28d46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb3e85bdbd22b18e2b467bf3fa79d143",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ec0da2fa94247d0a8ad",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b658d34d672251f9b42f",
+    "url": "/static/js/2.4f31d5e9.chunk.js"
+  },
+  {
+    "revision": "796e9647298eb54e8533fe00df5f7b2b",
+    "url": "/static/js/2.4f31d5e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ec0da2fa94247d0a8ad",
+    "url": "/static/js/main.709e8fb7.chunk.js"
+  },
+  {
+    "revision": "8cd05d15c083a861362f",
+    "url": "/static/js/runtime-main.16efb6ff.js"
+  }
+]);
